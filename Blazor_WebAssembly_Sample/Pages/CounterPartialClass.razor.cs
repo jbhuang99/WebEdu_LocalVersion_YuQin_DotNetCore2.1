5c1f351cb37228ecd26ab74726ac8f32bea7fb87
jbhuang99 <jbhuang99@hotmail.com>
2024-08-02 20:56:39 +0800

    projectsadded

---
+++ Blazor_WebAssembly_Sample/Pages/CounterPartialClass.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor_WebAssembly_Sample.Pages;
+
+public  partial class CounterPartialClass
+{
+   static private int currentCount = 0;
+
+  static  private void IncrementCount()
+    {
+        currentCount++;
+    }
+
+}
