5989fc348b9215aab99b3deeaa2e159448ecdd3b
YuQinPublishBlazorOnGithubPages <YuQinPublishBlazorOnGithubPages@outlook.com>
2024-09-14 18:40:43 +0800

    contents.html modified

--- ASPDotNet_MVC_YuQin/ASPDotNet_MVC_YuQin/wwwroot/js_from_ts/try/file.js
+++ ASPDotNet_MVC_YuQin/ASPDotNet_MVC_YuQin/wwwroot/js_from_ts/try/file.js
@@ -1,3 +1,20 @@
-window.alert("try文件夹");
-document.getElementById('s1').innerHTML = "try文件夹，I'm comming....";
+var TryNameSpace;
+(function (TryNameSpace) {
+    var TryClass = /** @class */ (function () {
+        function TryClass() {
+        }
+        TryClass.prototype.TryMothod = function () {
+            window.alert("TryNameSpace名称空间");
+            document.getElementById('s1').innerHTML = "TryNameSpace名称空间，I'm comming....";
+            return (2);
+        };
+        return TryClass;
+    }());
+    TryNameSpace.TryClass = TryClass;
+})(TryNameSpace || (TryNameSpace = {}));
+//var http = new HttpClient();
+//var uriBuilder = new URIBuilder("http://www.sogou.com/web");
+var tryClass = new TryNameSpace.TryClass();
+var str = "";
+tryClass.TryMothod();
 //# sourceMappingURL=file.js.map
--- ASPDotNet_MVC_YuQin/Blazor_WebAssembly_PWA_YuQin/wwwroot/ts/try/file.ts
+++ ASPDotNet_MVC_YuQin/Blazor_WebAssembly_PWA_YuQin/wwwroot/ts/try/file.ts
@@ -1,2 +1,16 @@
-﻿window.alert("try文件夹");
-document.getElementById('s1').innerHTML = "try文件夹，I'm comming...."
+﻿namespace TryNameSpace {
+    export class TryClass {
+        public TryMothod():Number {
+            window.alert("TryNameSpace名称空间");
+            document.getElementById('s1').innerHTML = "TryNameSpace名称空间，I'm comming...."
+            return (2);
+        }
+        public TryProperty:String;
+    }
+
+}
+//var http = new HttpClient();
+//var uriBuilder = new URIBuilder("http://www.sogou.com/web");
+var tryClass: TryNameSpace.TryClass  = new TryNameSpace.TryClass();
+var str: String = "";
+tryClass.TryMothod();
