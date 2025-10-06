955c4902ab645c12dea54fed6310bd7c960007d0
jbhuang99 <jbhuang99@hotmail.com>
2025-10-06 16:20:53 +0800

    iframeInitial

---
+++ ASPDotNet_MVC_YuQin/ASPDotNet_MVC_YuQin/wwwroot/webCourse/common/script/iframeInitial.js
@@ -0,0 +1,15 @@
+﻿        function fnSearchQueryString(){
+            alert("window.location");
+            alert(window.location.href);
+            alert(window.location.search);
+         var sSearch =new URLSearchParams(window.location.search);
+            if(sSearch.has("text")){
+         alert(document.getElementById("sIframeBook").src);
+        document.getElementById("sIframeBook").src=document.getElementById("sIframeBook").src+"?text="+sSearch.get("text");
+        alert(document.getElementById("sIframeBook").src);
+}
+else{
+//document.getElementById("sIframeBook").src="initial.html";
+alert(document.getElementById("sIframeBook").src);
+    }
+    }
