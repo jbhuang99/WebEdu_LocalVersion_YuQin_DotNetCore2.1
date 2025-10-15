4a1cc0df28619dc4ddcdca7084324df61a58b48f
jbhuang99 <jbhuang99@hotmail.com>
2025-10-15 23:47:27 +0800

    homeworkandtest

--- ASPDotNet_MVC_YuQin/ASPDotNet_MVC_YuQin/wwwroot/webCourse/common/script/HomeworkAndTest - Copy.js
+++ ASPDotNet_MVC_YuQin/ASPDotNet_MVC_YuQin/wwwroot/webCourse/common/script/HomeworkAndTest - Copy.js
@@ -1,15 +1,4 @@
-﻿function fnToggleEventSoureElementColor(){
-   // alert();
-    var eventSoureElementColor=window.event.target.style.color;
-    
-    if(eventSoureElementColor==""||eventSoureElementColor==null){
-        window.event.target.style.color="green";
-    }
-    else{
-        window.event.target.style.color="";
-    }
-}
-function fnToggleContentEditable(i) {
+﻿function fnToggleContentEditable(i) {
     
     var oTable = document.getElementsByTagName("table").item(0);
     var cTr = Array.from(oTable.rows);
@@ -31,7 +20,7 @@ function fnToggleContentEditable(i) {
     }
     }
     
-    for (j=i;j<i+8;j++){
+    for (j=i;j<i+7;j++){
 
     if (cTr[j+1].contentEditable == "true") 
     {
@@ -93,7 +82,7 @@ function fnToggleItemDisplay(i) {
     }
 
     
-    for (j=i;j<i+8;j++){
+    for (j=i;j<i+7;j++){
 
     if (cTr[j+1].style.display == "none") 
     {
@@ -129,25 +118,24 @@ function fnHomeworkAndTest() {
   var oTable = document.getElementsByTagName("table").item(0);var cTr = Array.from(oTable.rows); // 只包含主表的直接<tr>。解决上述包含嵌套表格的行。
    //alert(cTr[1].getElementsByTagName("td").item(0).innerHTML);
     var iTrLenth = cTr.length;
-   var iProblemNum = iTrLenth / 9;
+   var iProblemNum = iTrLenth / 8;
     //console.log(cTr[1].innerHTML);
    
     for (i = 0; i < iTrLenth; i++) {
         //for (j = 0; j < 6; j=i*j) {
-        if ((i + 9) % 9 === 0) { cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span style=\"display:inline-block; padding-top: 12px;padding-bottom: 12px;color:red\">题目" + ((i + 9) / 9) + "：<input type=\"button\" value=\"单击切换本题显示\" title='以便聚焦显示的题目，以便排除其他题目干扰！' onclick=\"fnToggleItemDisplay("+i+")\" /><input type=\"button\" value=\"单击切换本题在线编辑\" title='当前只是临时应急排版错误，但是无法保存编辑结果，必须重新上传当前条目的作业测验的.doc/.docx文档（作业测验的模板建议单击“标题框架”的“关于条目资源模板”下载）。后续将开发保存编辑结果的功能！' onclick=\"fnToggleContentEditable("+i+")\" /><input type=\"button\" value=\"单击登录服务端作业测验、统计分析（正在开发之中...）\" /></span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML; }
+        if ((i + 8) % 8 === 0) { cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span style=\"color:red\">题目" + ((i + 8) / 8) + "：<input type=\"button\" value=\"单击切换本题显示\" title='以便聚焦显示的题目，以便排除其他题目干扰！' onclick=\"fnToggleItemDisplay("+i+")\" /><input type=\"button\" value=\"单击切换本题在线编辑\" title='当前只是临时应急排版错误，但是无法保存编辑结果，必须重新上传当前条目的作业测验的.doc/.docx文档（作业测验的模板建议单击“标题框架”的“关于条目资源模板”下载）。后续将开发保存编辑结果的功能！' onclick=\"fnToggleContentEditable("+i+")\" /><input type=\"button\" value=\"单击登录服务端作业测验、统计分析（正在开发之中...）\" /></span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML; }
 
-        if ((i + 9) % 9 === 1) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 9) / 9)) + "\" />(A)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>"; }
-        if ((i + 9) % 9 === 2) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 9) / 9)) + "\" />(B)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>"; }
-        if ((i + 9) % 9 === 3) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 9) / 9)) + "\" />(C)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>"; }
-        if ((i + 9) % 9 === 4) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 9) / 9)) + "\" />(D)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>";}
+        if ((i + 8) % 8 === 1) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 8) / 8)) + "\" />(A)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>"; }
+        if ((i + 8) % 8 === 2) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 8) / 8)) + "\" />(B)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>"; }
+        if ((i + 8) % 8 === 3) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 8) / 8)) + "\" />(C)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>"; }
+        if ((i + 8) % 8 === 4) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"radio\" name=\"options" + sTimeStamp + Math.floor(((i + 8) / 8)) + "\" />(D)</span>" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span>";}
 
-        if ((i + 9) % 9 === 5) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"white-space: normal;color:red\"><input type=\"button\" title='（本机端作业与测试不支持成绩统计分析，服务端作业与测验可支持成绩统计分析！）（本机端作业与测试←可以登录服务端同步→服务端作业与测验！）' onclick=\"fnAnswerAndToggoleAnswerDisplay();\" value=\"查看本题答案 【注：（本机端作业与测试不支持成绩统计分析，服务端作业与测验可支持成绩统计分析！）（本机端作业与测试←可以登录服务端同步→服务端作业与测验！）】\" /></span>" + "<span style=\"display:none\" id=\"idAnswer" + sTimeStamp + Math.floor(((i + 9) / 9))  +"\""+">" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span></span>";}
-       if ((i + 9) % 9 === 6) { cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"white-space: normal;color:red\"><input type=\"button\" onclick=\"fnToggoleAnswerExplainDisplay();\" value=\"查看本题答案的解释 【注：建议基于本题答案解释的具体需求，超链接合适的字符（例如.docx文件、.pptx文件）-图像视频（例如.mp4文件）-2D（例如.svg文件）-3D（例如.x3d文件）】\" /></span>" + "<span style=\"display:none\" id=\"idExplain" + sTimeStamp + Math.floor(((i + 9) / 9)) + "\""+">" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span></span>"; }
-       if ((i + 9) % 9 === 7) { cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"white-space: normal;color:red\"><input type=\"button\" onclick=\"fnToggoleFourLayerLanguageDisplay();\" value=\"查看本题选用的“四层平台”的思维语言的解释【注：同一目录条目的作业测验，各个题目选用的思维语言，一般差别不大】\" /></span>" + "<span style=\"display:none\" id=\"idToggoleFourLayerLanguageDisplay" + sTimeStamp + Math.floor(((i + 9) / 9)) + "\""+">" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span></span>"; }
-        if ((i + 9) % 9 === 8) { cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"white-space: normal;color:red\"><input type=\"button\" onclick=\"fnToggoleFiveLayerMVCExplainDisplay();\" value=\"查看本题面向的“五层MVC”的解释【注：A、实践-数据读写封装；B、技术-信息提取运用；C、科学-规律预测探究；D、人文-情感交流共鸣；E、哲学-智能建构生成】\" /></span>" + "<span style=\"display:none\" id=\"idFiveLayerMVCExplain" + sTimeStamp + Math.floor(((i + 9) / 9)) + "\""+">" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span></span>"; }
+        if ((i + 8) % 8 === 5) {cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"button\" title='（本机端作业与测试不支持成绩统计分析，服务端作业与测验可支持成绩统计分析！）（本机端作业与测试←可以登录服务端同步→服务端作业与测验！）' onclick=\"fnAnswerAndToggoleAnswerDisplay();\" value=\"查看本题答案 【注：（本机端作业与测试不支持成绩统计分析，服务端作业与测验可支持成绩统计分析！）（本机端作业与测试←可以登录服务端同步→服务端作业与测验！）】\" /></span>" + "<span style=\"display:none\" id=\"idAnswer" + sTimeStamp + Math.floor(((i + 8) / 8))  +"\""+">" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span></span>";}
+       if ((i + 8) % 8 === 6) { cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"button\" onclick=\"fnToggoleAnswerExplainDisplay();\" value=\"查看本题答案的解释 【注：建议基于本题答案解释的具体需求，超链接合适的字符（例如.docx文件、.pptx文件）-图像视频（例如.mp4文件）-2D（例如.svg文件）-3D（例如.x3d文件）】\" /></span>" + "<span style=\"display:none\" id=\"idExplain" + sTimeStamp + Math.floor(((i + 8) / 8)) + "\""+">" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span></span>"; }
+        if ((i + 8) % 8 === 7) { cTr[i].getElementsByTagName("td").item(0).innerHTML = "<span><span style=\"color:red\"><input type=\"button\" onclick=\"fnToggoleFiveLayerMVCExplainDisplay();\" value=\"查看本题所属的五层MVC类别 【注：A、实践-数据读写封装；B、技术-信息提取运用；C、科学-规律预测探究；D、人文-情感交流共鸣；E、哲学-智能建构生成】\" /></span>" + "<span style=\"display:none\" id=\"idFiveLayerMVCExplain" + sTimeStamp + Math.floor(((i + 8) / 8)) + "\""+">" + cTr[i].getElementsByTagName("td").item(0).innerHTML + "</span></span>"; }
        // }
     }
-    //alert(cTr[9].getElementsByTagName("td").item(0).innerHTML + ";" + cTr[9].getElementsByTagName("td").item(0).innerHTML + ";" + cTr[10].getElementsByTagName("td").item(0).innerHTML);
+    //alert(cTr[8].getElementsByTagName("td").item(0).innerHTML + ";" + cTr[9].getElementsByTagName("td").item(0).innerHTML + ";" + cTr[10].getElementsByTagName("td").item(0).innerHTML);
     /**
      if(!("speechSynthesis" in window)) {throw alert("对不起，您的浏览器不支持");} 
      //alert(parent.document.getElementById("sFramesetContentAndHomeworkAndTest").rows=="0%,*");
@@ -164,17 +152,15 @@ function fnHomeworkAndTest() {
 
 
 function fnValidationHomeworkAndTest() {
-   
     try {
        // var cTr = document.getElementsByTagName("table").item(0).getElementsByTagName("tr");//最外层表格嵌套了表格（如题干有表格）的问题没有解决
         var oTable = document.getElementsByTagName("table").item(0);var cTr = Array.from(oTable.rows); // 只包含主表的直接<tr>。解决上述包含嵌套表格的行。
         var iTrLenth = cTr.length;
        // alert(iTrLenth);
-       // if (iTrLenth % 8 != 0) { alert("上传的作业与测试可能有问题，无法正常运行。请基于word模板文件，排版制作作业与测试，然后上传！每道题必须是9个表行。总表行必须是9的倍数。"); }
-       
-if (iTrLenth % 9 != 0) { alert("上传的作业与测试可能有问题，无法正常运行。请基于word模板文件，排版制作作业与测试，然后上传！每道题必须是9个表行。总表行必须是9的倍数。"); }
-      //  if (false) { alert("上传的作业与测试可能有问题，每道题必须是9个表行。第6个表行是答案，必须是A\B\C\D中的一个字母，不能有其他字符、空格！请基于word模板文件，排版制作作业与测试，然后上传！"); }//
-      if (false) { alert("上传的作业与测试可能有问题，每道题必须是9个表行。第6个表行是答案，必须是A\B\C\D中的一个字母，不能有其他字符、空格！。第8个表行是四层平台思维语言的解释。第9个表行是五层MVC的解释（A.实践-数据读写封装/B.技术-信息提取运用/C.科学-规律预测探究/D.人文-情感交流共鸣/E.哲学-智能建构生成），必须是A\B\C\D\E中的一个字母，不能有其他字符、空格！请基于word模板文件，排版制作作业与测试，然后上传！"); }//
+       // if (iTrLenth % 7 != 0) { alert("上传的作业与测试可能有问题，无法正常运行。请基于word模板文件，排版制作作业与测试，然后上传！每道题必须是7个表行。总表行必须是7的倍数。"); }
+if (iTrLenth % 8 != 0) { alert("上传的作业与测试可能有问题，无法正常运行。请基于word模板文件，排版制作作业与测试，然后上传！每道题必须是8个表行。总表行必须是8的倍数。"); }
+      //  if (false) { alert("上传的作业与测试可能有问题，每道题必须是7个表行。第6个表行是答案，必须是A\B\C\D中的一个字母，不能有其他字符、空格！请基于word模板文件，排版制作作业与测试，然后上传！"); }//
+      if (false) { alert("上传的作业与测试可能有问题，每道题必须是8个表行。第6个表行是答案，必须是A\B\C\D中的一个字母，不能有其他字符、空格！。第8个表行是层MVC的解释（A.实践-数据读写封装/B.技术-信息提取运用/C.科学-规律预测探究/D.人文-情感交流共鸣/E.哲学-智能建构生成），必须是A\B\C\D\E中的一个字母，不能有其他字符、空格！请基于word模板文件，排版制作作业与测试，然后上传！"); }//
     }
     catch (e) {
         alert("上传的作业与测试可能有问题，无法正常运行。请基于word模板文件，排版制作作业与测试，然后上传！");
@@ -182,7 +168,6 @@ if (iTrLenth % 9 != 0) { alert("上传的作业与测试可能有问题，无法
 }
 
 function fnAnswerAndToggoleAnswerDisplay() {
-    fnToggleEventSoureElementColor();
     document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display = "none";
     //alert(event.srcElement.parentElement.parentElement.getElementsByTagName("span").item(event.srcElement.parentElement.parentElement.getElementsByTagName("span").length - 1).innerHTML);
     // alert(event.srcElement.parentElement.parentElement.getElementsByTagName("span").item(event.srcElement.parentElement.parentElement.getElementsByTagName("span").length - 1).id);
@@ -231,7 +216,6 @@ function fnAnswerAndToggoleAnswerDisplay() {
 }
 
 function fnToggoleAnswerExplainDisplay() {
-    fnToggleEventSoureElementColor();
         if (document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display == "none") {
         document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display = "";
     }
@@ -240,17 +224,7 @@ function fnToggoleAnswerExplainDisplay() {
     }
 }
 
-function fnToggoleFourLayerLanguageDisplay() {
-    fnToggleEventSoureElementColor();
-        if (document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display == "none") {
-        document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display = "";
-    }
-    else {
-        document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display = "none";
-    }
-}
 function fnToggoleFiveLayerMVCExplainDisplay() {
-    fnToggleEventSoureElementColor();
         if (document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display == "none") {
         document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display = "";
     }
@@ -258,5 +232,4 @@ function fnToggoleFiveLayerMVCExplainDisplay() {
         document.getElementById(event.srcElement.parentElement.parentElement.children[1].id).style.display = "none";
     }
 }
-
 
