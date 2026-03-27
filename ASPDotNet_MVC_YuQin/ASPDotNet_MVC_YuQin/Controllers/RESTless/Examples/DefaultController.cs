0f345bc70dd82b7d7a4541feb6e256f3d1e2d889
jbhuang99 <jbhuang99@hotmail.com>
2026-03-28 00:03:32 +0800

    content modified

--- ASPDotNet_MVC_YuQin/ASPDotNet_MVC_YuQin/Controllers/RESTless/Examples/DefaultController.cs
+++ ASPDotNet_MVC_YuQin/ASPDotNet_MVC_YuQin/Controllers/RESTless/Examples/DefaultController.cs
@@ -7,13 +7,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebEdu_LocalVersion_YuQin_DotNetCore21.Controllers.Examples
 {
-    public class DefaultController : Controller
+
+    public class DefaultController : Controller//:有点像“像”的单重继承又要功能强大，
     {
         // GET: Default
+        public Int32 ID { set; get; }//3621.....
+        public Int32 ID2 { set; get; }//3621.....
         public ActionResult Index()
         {
             String myString = "public";
-            return Content(myString);
+            ID = 321;
+            ID2 = 1;
+            return Content(myString+(ID+ID2).ToString());
             // return Content("Hello world");
         }
 
