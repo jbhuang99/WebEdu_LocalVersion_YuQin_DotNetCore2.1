3e2a0586efa3f8c4fec098ab70807a9978d50407
jbhuang99 <jbhuang99@hotmail.com>
2026-09-19 11:56:41 +0800

    dotNF

---
+++ ASPDotNet_MVC_YuQin/DotNFApp1/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace YuQin.DotNFApp1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello from nanoFramework!");
+
+            Thread.Sleep(Timeout.Infinite);
+
+            // Browse our samples repository: https://github.com/nanoframework/samples
+            // Check our documentation online: https://docs.nanoframework.net/
+            // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+    }
+}
